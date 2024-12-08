7d8bab9df4431e28e72c1d79c97095ef44081a91
KEREM CELIK <KEREM CELIK@DESKTOP-3EQDEM4>
2024-12-08 21:31:44 +0300

    Add project files.

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repositories;
+using RedisExampleApp.API.Services;
+using RedisExampleApp.Cache;
+using StackExchange.Redis;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //private readonly IDatabase _database;
+
+        //private readonly RedisService _redisService;
+
+
+
+        //public ProductsController(IProductService productService/*, RedisService redisService*//*, IDatabase database*/)
+        //{
+        //    _productService = productService;
+            //_database = database;
+            //_redisService = redisService;
+            //var db = _redisService.GetDb(0);
+            //db.StringSet("isim", "ahmet");
+
+            //_database.StringSet("soyad", "yıldız");
+        //}
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productService.GetAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _productService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            return Created(string.Empty, await _productService.CreateAsync(product));
+        }
+    }
+}
